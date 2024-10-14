95d85687d6bff1223df4e8cd585edea3216f8995
Happy2018new <chenbo79800@163.com>
2024-10-14 19:06:30 +0800

    Minor fix on `Raknet` struct define

--- core/raknet/wrapper/define.go
+++ core/raknet/wrapper/define.go
@@ -10,8 +10,7 @@ import (
 	"Eulogist/core/minecraft/protocol/packet"
 )
 
-// 描述一个简单的，但可以支持不同 Minecraft
-// 通信协议的基本 Raknet 连接实例
+// 描述一个基本的 Raknet 连接实例
 type Raknet struct {
 	connection net.Conn
 	shieldID   atomic.Int32
@@ -35,6 +34,11 @@ type Raknet struct {
 type MinecraftPacket struct {
 	// 保存已解码的 Minecraft 数据包
 	Packet packet.Packet
-	// 保存数据包的二进制形式
+	// 保存数据包的二进制形式。
+	//
+	// 在发送 MinecraftPacket 时，
+	// 将优先采用该字段，除非该字段为空，
+	// 则将会尝试编码 Packet 字段，
+	// 然后将编码结果发送到 Raknet 上
 	Bytes []byte
 }
